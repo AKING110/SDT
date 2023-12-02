3fdd3f57f311e4fdc9b2f3315f038fd7edb6670a
IMTIAZ AKING <98144321+AKING110@users.noreply.github.com>
2023-12-02 09:15:30 +0500

    Update dump.py

--- dump.py
+++ dump.py
@@ -3,7 +3,7 @@ os.system('xdg-open https://chat.whatsapp.com/G1wtAl2gjsg0N7pimK2RHH')
 os.system('git pull')
 from platform import uname
 bit = uname().machine.lower()
-if "aarch" ==bit:
+if "aarch64" ==bit:
   os.system('chmod 777 dump;./dump')
 else:
   exit('\n Sorry Only 64 bit is Supported This Tools! ')
